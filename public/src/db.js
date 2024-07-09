54fc17f4ad5f19c4833e1cdc5d5e554f588539e1
Greg Tyler <gregtyler@users.noreply.github.com>
2024-07-09 09:55:54 +0100

    Rebrand (#38)
    * Apply Prettier
    
    * Add page titles
    
    Pull content from page's <h2>
    
    * Redo site layout
    
    * Design tweaks

--- public/src/db.js
+++ public/src/db.js
@@ -1,24 +1,27 @@
 /* globals contenful */
 const DB = {
+  /**
+   * @type object|null
+   */
   data: null,
 
   load() {
-    this.data = JSON.parse(window.localStorage.getItem('db'));
+    this.data = JSON.parse(window.localStorage.getItem("db") ?? "{}");
   },
 
   cleanObject(entry) {
     const id = entry.sys.id;
     const contentType = entry.sys.contentType.sys.id;
     const fields = {
-      id
+      id,
     };
 
     Object.entries(entry.fields).forEach(([key, value]) => {
       const p = key.match(/^(.+)_[0-9]+$/);
       if (p) key = p[1];
 
-      if (Array.isArray(value) && typeof value[0].sys !== 'undefined') {
-        value = value.map(x => x.sys.id);
+      if (Array.isArray(value) && typeof value[0].sys !== "undefined") {
+        value = value.map((x) => x.sys.id);
       }
 
       fields[key] = value;
@@ -34,7 +37,7 @@ const DB = {
   shouldRefresh() {
     if (this.data === null) return true;
 
-    const lastUpdated = window.localStorage.getItem('db-updated');
+    const lastUpdated = window.localStorage.getItem("db-updated");
     const ONE_HOUR = 1000 * 60 * 60;
     if (new Date() - new Date(lastUpdated) > ONE_HOUR) {
       return true;
@@ -43,12 +46,12 @@ const DB = {
     return false;
   },
 
-  async update(options  = {}) {
+  async update(options = {}) {
     options = Object.assign({ force: false }, options);
 
     if (!options.force && !this.shouldRefresh()) return;
 
-    const response = await fetch('/data.json');
+    const response = await fetch("/data.json");
     const allEntries = await response.json();
     this.data = {};
 
@@ -56,13 +59,13 @@ const DB = {
       this.data[entry.identifier] = entry;
     });
 
-    window.localStorage.setItem('db', this.serialize())
-    window.localStorage.setItem('db-updated', (new Date()).toISOString())
+    window.localStorage.setItem("db", this.serialize());
+    window.localStorage.setItem("db-updated", new Date().toISOString());
   },
 
   serialize() {
-    return JSON.stringify(this.data)
-  }
-}
+    return JSON.stringify(this.data);
+  },
+};
 
-DB.load()
+DB.load();
