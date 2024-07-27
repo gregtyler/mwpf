ad7aba8395e24fd9aff713866f82f3a7be5cd1da
Greg Tyler <gregtyler@users.noreply.github.com>
2024-07-27 13:53:25 +0100

    Download and shrink images (#42)
    * Rename queries->DB
    
    * Download and shrink images
    
    Rather than hot-loading images from directly from contentful, download a copy of them locally and resize to an appropriate width.
    
    This gives us more control over network requests, and ensures we don't load images that are too large

---
+++ src/lib/resizeFilter.mjs
@@ -0,0 +1,40 @@
+import path from "path";
+import fs from "fs";
+import sharp from "sharp";
+
+export function buildResizeFilter(baseDir) {
+  return async function resizeFilter(thumbnailURL, width) {
+    // Calculate file name
+    const extension = path.extname(thumbnailURL);
+    const newName = `${path.basename(thumbnailURL, extension)}-${width}${extension}`;
+    const targetPath = path.resolve(baseDir, "images", newName);
+
+    // Check if the work is already done
+    if (fs.existsSync(targetPath)) {
+      return `/images/${newName}`;
+    }
+
+    // Fetch image
+    if (thumbnailURL.startsWith("//")) thumbnailURL = `https:${thumbnailURL}`;
+    const response = await fetch(thumbnailURL);
+
+    // Resize image
+    const imageBuffer = await readableStreamToBuffer(response.body);
+    await sharp(imageBuffer)
+      .resize(width)
+      .toFile(path.resolve(baseDir, "images", newName));
+
+    // Return file name
+    return `/images/${newName}`;
+  };
+}
+
+async function readableStreamToBuffer(readableStream) {
+  const buffers = [];
+
+  for await (const data of readableStream) {
+    buffers.push(data);
+  }
+
+  return Buffer.concat(buffers);
+}
